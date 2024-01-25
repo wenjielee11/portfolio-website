543657d16b4d51560fdcd4ce8f9d48a3cf437be3
wenjielee11 <leewenjie.wjl@gmail.com>
2024-01-25 05:28:48 -0600

    Build succesful, adding website title

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,8 +2,8 @@ import { Layout } from '@/components/dom/Layout'
 import '@/global.css'
 
 export const metadata = {
-  title: 'Next.js + Three.js',
-  description: 'A minimal starter for Nextjs + React-three-fiber and Threejs.',
+  title: 'wenjielee.tech',
+  description: 'A blog on programming, software development and thoughts.',
 }
 
 export default function RootLayout({ children }) {
