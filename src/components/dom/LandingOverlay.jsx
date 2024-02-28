60dc8b312d44dde5ac8bc73f482287226fb10275
wenjielee11 <leewenjie.wjl@gmail.com>
2024-02-28 02:14:44 -0600

    Changed alert position

--- src/components/dom/LandingOverlay.jsx
+++ src/components/dom/LandingOverlay.jsx
@@ -64,7 +64,7 @@ const LandingOverlay = () => {
             {showAlert && (
                 <div style={{
                     position: 'fixed',
-                    top: 0,
+                    bottom: 0,
                     left: 0,
                     right: 0,
                     backgroundColor: 'black',
