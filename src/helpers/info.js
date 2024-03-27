7a840e26deed94ac2c0db6a0012d28368d78dd97
wenjielee11 <96722108+wenjielee11@users.noreply.github.com>
2024-03-26 19:21:00 -0500

    Update info.js

--- src/helpers/info.js
+++ src/helpers/info.js
@@ -21,7 +21,7 @@ export const Socials = [
     {
         name: "Linkedin",
         src: "/icons/linkedin.png",
-        link: "https://linkedin.com/in/lee-wen-jie-042baa267",
+        link: "www.linkedin.com/in/wen-jie-lee-042baa267",
     },
     {
         name: "github",
